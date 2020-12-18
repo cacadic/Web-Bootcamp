0319f24c13b1265aed9a39704d4863a8bb482aa7
cacadic <phamviethonglinh@gmail.com>
2020-12-18 10:06:03 +0700

    Udemy Bootcamp 148

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+var ul = document.querySelector("ul");
+var li = document.querySelector("li a");
+var btn = document.querySelector(".btn");
+
+ul.lastElementChild.innerHTML = "Lĩnh Phạm";
+li.style.color = "red";
+btn.style.backgroundColor = "yellow";
