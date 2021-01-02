130fa832c3b170f42dd38a2a86eaf579e7bff3d4
Linh Pham <phamviethonglinh@gmail.com>
2021-01-02 14:29:03 +0700

    Udemy Bootcamp 249

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,11 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
+
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
 
+app.post("/", function (req, res) {
+    const firstName = req.body.fName,
+        lastName = req.body.lName,
+        email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us7.api.mailchimp.com/3.0/lists/4d04971947";
+
+    const options = {
+        method: "POST",
+        auth: "linhpham:c6e41f757be8eabb23edb0271be221fe-us7"
+    }
+
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+//API Key: c6e41f757be8eabb23edb0271be221fe-us7
+//List ID: 4d04971947
