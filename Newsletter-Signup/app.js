6e7444ffe271a94085b705cdb6fa53fef55ae1a7
Linh Pham <phamviethonglinh@gmail.com>
2021-01-02 17:41:31 +0700

    Udemy bootcamp 251

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -36,10 +36,15 @@ app.post("/", function (req, res) {
 
     const options = {
         method: "POST",
-        auth: "linhpham:c6e41f757be8eabb23edb0271be221fe-us7"
+        auth: "linhpham:c6e41f757be8eabb23edb0271be221fe-us7z"
     }
 
     const request = https.request(url, options, function (response) {
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         })
@@ -49,7 +54,11 @@ app.post("/", function (req, res) {
     request.end();
 });
 
-app.listen(3000, function(){
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on port 3000");
 });
 
